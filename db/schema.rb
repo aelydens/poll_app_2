dbfe09883c51ffe897b64797496e184e1e06ffc5
Annie Lydens <aelydens@gmail.com>
2015-03-09 15:56:35 -0600

    Create users migration and create sessions controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309172802) do
+ActiveRecord::Schema.define(version: 20150309214146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,14 @@ ActiveRecord::Schema.define(version: 20150309172802) do
   end
 
   add_index "questions", ["poll_id"], name: "index_questions_on_poll_id", using: :btree
+
+  create_table "users", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "polls"
