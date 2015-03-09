13252a884717bcd70dd6896d13dccd5ad91d59ed
Annie Lydens <aelydens@gmail.com>
2015-03-09 15:37:50 -0600

    Initial setup for github authentication

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resources :answers
     end
   end
-
+  get "/auth/:provider/callback", to: "sessions#create"
+  delete "/signout", to: "sessions#destroy", as: :signout
   root 'polls#index'
 end
