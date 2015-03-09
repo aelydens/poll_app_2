472a9486e79cbb27856a179b0b13a0f7a2b47455
Annie Lydens <aelydens@gmail.com>
2015-03-09 12:04:50 -0600

    Flesh out CRUD actions for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -17,8 +17,33 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params)
+      flash[:success] = "Question successfully updated"
+      redirect_to questions_path
+    else
+      render :dit
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+    if @question.destroy
+      flash[:success] = "Question successfully removed"
+    else
+      flash[:error] = "There was an error removing this question"
+    end
+    redirect_to questions_path
+  end
+  
   private
 
   def question_params
-    params.require(:question).permit(:question, :poll_id)
+    params.require(:question).permit(:question, :poll_id, answer_attributes: [:id, :answer, :_destroy])
+  end
 end
